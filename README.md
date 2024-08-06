451d6aabcd981f6d0708434b8560056455680f28
Chris Natale <natale565@icloud.com>
2024-08-06 18:05:21 -0400

    fixed JS and shapes JS

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# svg_logo_maker
+# SVG Logo Maker
+
+# Description
+
+- This application was created to allow the user to create simple logos in a SVG file using a Node.js command-line application.
+
+- The application uses inquirer to prompt the user within the command line with questions on how they would like their logo to look (what text they would like their logo to display (up to 3 characters in length), the color of the text, the shape of their logo (triangle, square, or circle) and the color of that shape.) Once the user answers all prompts, then an SVG file is written using their selections to generate a logo.
+
+
+# Live Screen Recording Of Application 
+
+- [Click here to watch video](https://drive.google.com/file/d/1pSxstOD3gOP0YHF5s8NlMXHOBaXO5grM/view)
+
+# Screenshots
+
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { Circle, Square, Triangle } = require("./lib/shapes.js");
 
 inquirer.prompt([
     {
         type: "input",
         name: "text",
         message: "Enter up to 3 characters for the text of your logo",
+        validate: function(input) {
+            if (input.length === 3) {
+                return true;
+            }
+            return "Text must be exactly 3 characters long.";
+        }
     },
     {
         type: "input",
@@ -32,24 +39,22 @@ inquirer.prompt([
         if (err) {
             console.error("Error generating logo.svg:", err);
         } else {
-            console.log("Generated logo.svg");
+            console.log("logo.svg Was Generated!");
         }
     });
 });
-
-
 
 function generateSVG(text, textColor, shape, shapeColor) {
     let shapeSVG = "";
     switch (shape) {
         case "circle":
-            shapeSVG = `<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${shapeColor}" />`;
+            shapeSVG = new Circle(shapeColor).generateShapeSVG();
             break;
         case "triangle":
-            shapeSVG = `<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${shapeColor}" />`;
+            shapeSVG = new Triangle(shapeColor).generateShapeSVG();
             break;
         case "square":
-            shapeSVG = `<rect x="50" height="200" width="200" fill="${shapeColor}" />`;
+            shapeSVG = new Square(shapeColor).generateShapeSVG();
             break;
     }
 
